c742abf14f3e023a41a93520212d16efb527598e
AnPeach <nastyapisarenko2001@gmail.com>
2021-05-25 19:38:27 +0300

    Исправила синтаксические ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -143,7 +143,7 @@ const server = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html", gulp.series("html", sync.reload));
 };
 // Build
